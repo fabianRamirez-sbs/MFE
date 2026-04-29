066da960bb53a2e8eeaa191bef6f0402b54a2b8c
fabianRamirez-sbs <fabian.ramirez@sbseguros.co>
2026-04-28 22:48:45 -0500

    feat: actualizar scripts de desarrollo y agregar scripts para liberar puertos

--- tools/scripts/dev-all.sh
+++ tools/scripts/dev-all.sh
@@ -17,7 +17,7 @@
 
 set -uo pipefail
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
 APPS_DIR="$ROOT_DIR/apps"
 NVM_SCRIPT="$HOME/.nvm/nvm.sh"
 
